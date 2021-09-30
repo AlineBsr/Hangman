a8482904c423ef758daada97a3b10a7466d5a221
AlineBsr <bosser.aline@gmail.com>
2021-09-30 13:12:14 +0200

    js V0.1

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,7 +11,8 @@ for (let i = 0; i < alpha.length; i++) { letters.innerHTML += "<button id= btn" 
 let continueGame = 1;                                                                                  // SI continueGamme...
 
 let lettersGuess = "";                                                                                      // recup les lettres deja trouvées pour la regex
-let count = 0;                                                                                              // compte le nombre de raté
+let count = 0;               
+                                                                               // compte le nombre de raté
 for (let i = 0; i < letters.children.length; i++) {                                                         // pour ch elem boutons
     const btn = letters.children[i];                                                                        // stocke l'ind du bouton 
     const btnLetter = btn.textContent;                                                                      // stocke la lettre du bouton
@@ -22,34 +23,40 @@ for (let i = 0; i < letters.children.length; i++) {                             
             if ((document.getElementById("btn" + (i + 1)).getAttribute("find")) === "1") { alert("letter already found!") }         // SI la lettre n'a pas déjà été trouvée
             if ((document.getElementById("btn" + (i + 1)).getAttribute("find")) === "0") { alert("Letter has already been tested!") }// SI la lettre n'a pas déjà été testée 
 
-            if (wordToDevine.includes(btnLetter)) {                                                         // SI la lettre est dans le mot caché
+            if ( (wordToDevine.includes(btnLetter)  && (count <= 10)  ) ){                                                         // SI la lettre est dans le mot caché
                 wordH.innerHTML = "";                                                                       // init de l'affichahe du wordH
                 lettersGuess += btnLetter;                                                                  // stocke la lettre
                 wordH.innerHTML += wordToDevine.replace(RegExp("[^" + lettersGuess + "]", "g"), "_ ");      // remplace le _ par la lettre trouvée
-                document.getElementById("btn" + (i + 1)).style.backgroundColor = "green";                   // def style
-                document.getElementById("btn" + (i + 1)).style.color = "white";                             // def style
-                document.getElementById("btn" + (i + 1)).setAttribute("find", "1");                         // Ajout attr 'find' = true
+                if( wordH.getAttribute("win") !== "0"){                                                     // SI le mot n'a pas été trouvé
+                    document.getElementById("btn" + (i + 1)).style.backgroundColor = "green";               // def style
+                    document.getElementById("btn" + (i + 1)).style.color = "white";                         // def style
+                    document.getElementById("btn" + (i + 1)).setAttribute("find", "1");                     // Ajout attr 'find' = true
+                }
             }
             else    {                                                                                       // SINON           
-                if ((wordH.textContent !== wordToDevine) && (count <= 10)){
+                if ((wordH.textContent !== wordToDevine) && (count < 10)){
                     count += 1;                                                                             // incrémentation du count
-                    if (count < 10) { document.querySelector("img").setAttribute("src", "./assets/img/Hang" + String(count) + ".png"); } // ajout img pendu
+                    document.querySelector("img").setAttribute("src", "./assets/img/Hang" + String(count) + ".png"); // ajout part pendu
                     document.getElementById("btn" + (i + 1)).style.color = "white";                         // def style
                     document.getElementById("btn" + (i + 1)).style.backgroundColor = "red";                 // def style
                     document.getElementById("btn" + (i + 1)).setAttribute("find", "0");                     // ajout attr 'find' = false
                 }
             }
-            if (wordH.textContent === wordToDevine ){                                                       // SI le mot caché est strictement égal au mot à trouver
-                alert("YOU WIN !");                                                                         // alert WIN
-                continueGame=0;
-                wordH.setAttribute("win","1");                                                                             // fin du jeu
-            }
             if ( count === 10) {                                                                            // SI le count atteint 10
+                continueGame=0;                                                                             // fin du GAME
                 alert("YOU LOOSE !");                                                                       // alert LOOSE
-                continueGame=0;               
-                wordH.setAttribute("win","0");                                                                             // fin du jeu
-                                                                              // fin du jeu
+                wordH.setAttribute("win","0");
+                wordH.textContent = wordToDevine;                                                           // Affichage du mot
+                wordH.style.color = "red"                                                                   // en rouge
+
+            }
+            if (wordH.textContent === wordToDevine ){                                                       // SI le mot caché est strictement égal au mot à trouver
+                continueGame=0;                                                                             // fin du GAME
+                alert("YOU WIN !");                                                                         // alert WIN
+                wordH.setAttribute("win","1");
+                wordH.style.color = "green";                                                                // style
             }
         })
+       
     }
 }
